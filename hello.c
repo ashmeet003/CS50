7da343bfefe082f9a30880927ff4c3da1da2915d
Ashmeet Kaur <ashmeet-kaur1@myseneca.ca>
2025-05-29 23:02:59 -0400

    new lesson

--- hello.c
+++ hello.c
@@ -1,5 +1,11 @@
-#include <stdio.h>
+//#include <stdlib.h>
+#include <stdio.h> //standard io header file
+//header file : contains functionality which comes with the language
+// gives access to libraries
 int main(void)
 {
-    printf("hello, world\n");
+    //string answer=get_string("whats your name? ");
+    //printf("hello, world. Name is %s\n", answer);
+    printf("hello, world.");
+    
 }
