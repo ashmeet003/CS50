e16573cdc1b0620d8dc54a98641fdac25f64ad0a
Ashmeet Kaur <ashmeet-kaur1@myseneca.ca>
2025-05-30 17:01:12 -0400

    intended

--- compare.c
+++ compare.c
@@ -5,19 +5,19 @@ int main(void){
     scanf("%d", &option);
     if(option == 1){
         int x,y;
-    printf("Enter first number: ");
-    scanf("%d", &x);
-    printf("Enter second number: ");
-    scanf("%d",&y);
-    if(x>y){
-        printf("%d is greater than %d", x, y);
-    }
-    else if(x<y){
-        printf("%d number is greater than %d", y, x);
-    }
-    else{
-        printf("Both numbers are equal");
-    }
+        printf("Enter first number: ");
+        scanf("%d", &x);
+        printf("Enter second number: ");
+        scanf("%d",&y);
+        if(x>y){
+            printf("%d is greater than %d", x, y);
+        }
+        else if(x<y){
+            printf("%d number is greater than %d", y, x);
+        }
+        else{
+            printf("Both numbers are equal");
+        }
     }
     else if(option == 2){
         char x;
