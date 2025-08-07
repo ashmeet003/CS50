46fe520aa55d6f5699a9f574985f0de390e627ea
Ashmeet Kaur <ashmeet-kaur1@myseneca.ca>
2025-08-06 21:06:15 -0700

    enum and typedef added

---
+++ fromBroCode/practiseCode/enumExample.c
@@ -0,0 +1,31 @@
+/*
+    The program is meant to showcase the use of enum (short of enumerations)
+    and how it improves program's readability instead of using numbers
+*/
+
+#include <stdio.h>
+
+typedef enum{
+    SUCCESS, PENDING, CANCELLED //default value of 1st constant is 0 and so on
+}Status; //enum typedef is used and named Status (capital S)
+
+void checkStatus(Status status);
+
+int main(){
+    Status status = SUCCESS; //variable is named status(lower case s) and is assigned SUCCESS constant
+    checkStatus(status);
+}
+
+void checkStatus(Status status){ //status holds value as SUCCESS(also 0 in behind the scene)
+    switch(status){ //status is checked to match value
+        case SUCCESS:
+            printf(" Connection is successful!");
+            break;
+        case PENDING:
+            printf("Connection is Pending! Please Wait.");
+            break;
+        case CANCELLED:
+            printf("Connection is Cancelled! Sorry.");
+            break;
+    }
+}
