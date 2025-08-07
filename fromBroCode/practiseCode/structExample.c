64ef4234702e0e0417346f35f8f02f2df76eaf07
Ashmeet Kaur <ashmeet-kaur1@myseneca.ca>
2025-08-07 15:21:56 -0700

    struct example commented

--- fromBroCode/practiseCode/structExample.c
+++ fromBroCode/practiseCode/structExample.c
@@ -1,6 +1,11 @@
+/*
+    The program is designed to explain structs.
+    A student struct array is introduced to hold value for a number of students.
+    and displayed using function call.
+*/
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
+#include <string.h>  //used for strcpy
+#include <stdbool.h> //needed for bool value
 
 typedef struct{
     char name[50];
@@ -17,14 +22,22 @@ int main(){
     student[0] = (Student){"James", 18, 3.7, true};
     student[1] = (Student){"Allen", 18, 3.5, false};
     student[2] = (Student){"Elena", 17, 3.9, true};
+    // the values needs to be casted with (Student) since value are assigned after declaration and initialization
+    // also called compound literal
 
     student[3] = (Student){0};
-    strcpy(student[3].name, "Ved");
+    strcpy(student[3].name, "Ved"); //no cast was used here since value assigning is from data type to data type
     student[3].age = 19;
     student[3].gpa = 3.4;
     student[3].isFullTime = false;
 
-    int size = sizeof(student) / sizeof(student[0]);
+    /*  Another way for strcpy: safer alternative ; useful in buffer overflow situation
+        strncpy(student.name, "Ved", sizeof(student.name) - 1);
+        student.name[sizeof(student.name) - 1] = '\0';  // Ensure null termination
+
+    */
+
+    int size = sizeof(student) / sizeof(student[0]); //size of array student[]
 
     for(int i = 0; i < size; i++){
         displayStudent(student[i]);
