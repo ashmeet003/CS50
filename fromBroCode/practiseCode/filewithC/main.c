92500ba95a01bc0daa62e173fc8dc8329ec4f50f
Ashmeet Kaur <ashmeet-kaur1@myseneca.ca>
2025-08-07 22:24:22 -0700

    FILE using C

---
+++ fromBroCode/practiseCode/filewithC/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h> //contains inbuilt struct FILE
+
+int main(){
+
+    FILE *pFile = fopen("output.txt", "w"); //w = write & r = read
+    char text[] = "This is written in file.\nHave a good day!!!\n";
+    int numText = 123456;
+
+    if(pFile == NULL){ //checks if file was created
+        printf("File couldn't be opened!\n");
+        return 1;
+    }
+
+    printf("File was opened successfully!\n");
+    fprintf(pFile, "%s", text); //prints text in file
+    fprintf(pFile, "%d", numText);
+
+    fclose(pFile); //file must be closed
+
+    return 0;
+}
