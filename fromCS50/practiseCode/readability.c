09e16d565422b28792a104f60e3f62db814bafdc
Ashmeet Kaur <ashmeet-kaur1@myseneca.ca>
2025-08-12 15:18:37 -0700

    readability program added

--- fromCS50/practiseCode/readability.c
+++ fromCS50/practiseCode/readability.c
@@ -1,6 +1,22 @@
+/*
+    The program is based on readability test: developed over the years that define
+    formulas for computing the reading level of a text.
+    This test is based on: Coleman-Liau index
+        index = 0.0588 * L - 0.296 * S - 15.8
+    where L is the average number of letters per 100 words in the text, 
+    and S is the average number of sentences per 100 words in the text.
+
+    Example:
+        Would you like them here or there? 
+        I would not like them here or there.
+        I would not like them anywhere.
+        Output: Grade 2
+*/
+
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
+#include <ctype.h> //used for toupper()
+#include <math.h> //used for round();
 
 int main(){
 
@@ -8,61 +24,39 @@ int main(){
     printf("Enter your sentence: ");
     getchar();
     fgets(sentence, sizeof(sentence), stdin);
-    sentence[strlen(sentence) - 1] = '\0';
+    int strLen = strlen(sentence);
+    sentence[strLen - 1] = '\0'; //adds nul to last position instead of new line character
 
-    int numWords = 0;
+    int numWords = 1; //stores number of words in the string assuming str has atleast 1 word
+    double sumNumSen = 0; //total number of sentences ; double is imp for later calculation for S;
+    double sumNumLet = 1; //total number of letters ; double is imp for later calculation for S
+    //P.S. sum of items remain same regardless the denominator for calculating average
 
-    for(int i = 0; i < (strlen(sentence) - 1); i++){
+    for(int i = 0; i < strLen; i++){
         sentence[i] = toupper(sentence[i]);
-        if(sentence[i] == ' '){
+        if(sentence[i] == ' '){ //if a space is found; a number is added
             numWords++;
         }
-    }
-    numWords += 1;
-    int perHund = 0;
-
-    if(numWords > 100){
-        perHund = numWords / 100;
-    }
-    perHund += 1;
-
-    int avgNumLet[perHund];
-    int avgNumSen[perHund];
-    int posSent = 0;
-    int posLet = 0;
-    numWords = 1;
-    for(int i = 0; i < (strlen(sentence) - 1); i++){
-        if(sentence[i] == ' '){
-            numWords++;
+        else if(sentence[i] == '.' || sentence[i] == '?' || sentence[i] == '!'){// marks end of sentence
+            sumNumSen += 1; // adds number of sentences
         }
-        else if(sentence[i] == '.' || sentence[i] == '?' || sentence[i] == '!'){
-            avgNumSen[posSent] += 1;
-        }
-        else if(sentence[i] >= 'A' && sentence[i] <= 'Z'){
-            avgNumLet[posLet] += 1;
-        }
-        if(numWords == 100){
-            posSent++;
-            posLet++;
-            numWords = 1;
+        else if(sentence[i] >= 'A' && sentence[i] <= 'Z'){ //calculates number of letters
+            sumNumLet += 1; // adds number of letters
         }
     }
-    int L = 0, S = 0;
-    int sumL = 0, sumS = 0;
-    for(int i = 0; i < perHund; i++){
-        sumL += avgNumLet[i];
-        sumS += avgNumSen[i];
-    }
-    L = sumL / perHund;
-    S = sumS / perHund;
 
-    int index = 0.0588 * L - 0.296 * S - 15.8;
 
+    //marks start of implementing the formula
+    double L = (sumNumLet/ numWords) *100.00;  // calculates average number of letters per 100 words
+    double S = (sumNumSen / numWords) *100.00; // calculates average number of sentences per 100 words
+    double index = round((0.0588 * L) - (0.296 * S) - 15.8);  // Coleman-Liau index
+
+    //prints output
     if(index < 1){
-        printf("Before Grade1\n");
+        printf("Before Grade 1\n");
     }
     if(index >= 1 && index < 16){
-        printf("Grade %d\n", index);
+        printf("Grade %d\n", (int)(index));
     }
     if(index >= 16){
         printf("Grade 16+\n");
