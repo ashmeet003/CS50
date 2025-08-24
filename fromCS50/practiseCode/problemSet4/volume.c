8c7a3e79397d3d7b5d15d866ac307bea44d1690d
Ashmeet Kaur <ashmeet-kaur1@myseneca.ca>
2025-08-24 16:47:40 -0700

    volume file added

---
+++ fromCS50/practiseCode/problemSet4/volume.c
@@ -0,0 +1,58 @@
+// gcc volume.c -o volume
+// execution: ./volume.exe input.wav output.wav 0.5 //0.5/2/or any other factor by which sound would be toned
+// Modifies the volume of an audio file
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Number of bytes in .wav header
+const int HEADER_SIZE = 44;
+
+int main(int argc, char *argv[])
+{
+    // Check command-line arguments
+    if (argc != 4)
+    {
+        printf("Usage: ./volume input.wav output.wav factor\n");
+        return 1;
+    }
+
+    // Open files and determine scaling factor
+    FILE *input = fopen(argv[1], "rb"); //rb is required since .wav works on binary system
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "wb");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    float factor = atof(argv[3]);
+
+    // TODO: Copy header from input file to output file
+    uint8_t header[HEADER_SIZE];
+    fread(header, HEADER_SIZE, 1, input);
+    fwrite(header, HEADER_SIZE, 1, output);
+
+    // TODO: Read samples from input file and write updated data to output file
+    // Create a buffer for a single sample
+    int16_t buffer;
+    // Read single sample into buffer
+    while (fread(&buffer, sizeof(int16_t), 1, input) != 0)
+    {
+        // Update volume of sample
+        buffer *= factor;
+        // Write updated sample to new file
+        fwrite(&buffer, sizeof(int16_t), 1, output);
+    }
+
+    // Close files
+    fclose(input);
+    fclose(output);
+}
